c426aa92cbfb3e7caedb335e47a87307e08fa0fc
Dean321 <parvathi.saxena@gmail.com>
2022-02-26 20:43:37 +0530

    Update config.js

--- Attendance-app/config.js
+++ Attendance-app/config.js
@@ -2,14 +2,7 @@ import firebase from 'firebase';
 
 try {
   firebase.initializeApp({
-    apiKey: 'AIzaSyCon1XAe8Eqbd12pd4GtDD5PlHTlmP2k0g',
-    authDomain: 'wily-447d3.firebaseapp.com',
-    databaseURL:
-      'https://wily-447d3-default-rtdb.europe-west1.firebasedatabase.app',
-    projectId: 'wily-447d3',
-    storageBucket: 'wily-447d3.appspot.com',
-    messagingSenderId: '807353103797',
-    appId: '1:807353103797:web:574cd3dae8eb2dc69595fc',
+   // Add your config here
   });
 } catch (err) {
   if (!/already exists/.test(err.message)) {
