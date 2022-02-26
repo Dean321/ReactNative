5626ad7ff97ef842c058c049ef02b782e6066733
Dean321 <parvathi.saxena@gmail.com>
2022-02-26 13:30:48 +0530

    Update config.js

--- Willy/config.js
+++ Willy/config.js
@@ -2,12 +2,7 @@ import firebase from 'firebase';
 require("firebase/firestore");
 
 var firebaseConfig = {
-      apiKey: "AIzaSyCon1XAe8Eqbd12pd4GtDD5PlHTlmP2k0g",
-      authDomain: "wily-447d3.firebaseapp.com",
-      projectId: "wily-447d3",
-      storageBucket: "wily-447d3.appspot.com",
-      messagingSenderId: "807353103797",
-      appId: "1:807353103797:web:c9ff7bd66c91aa1e9595fc"
+     //Add your config here
     }
 
 firebase.initializeApp(firebaseConfig)
