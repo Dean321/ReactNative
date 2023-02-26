cb19e547851f1b6da289c4b7a45d4dde4ff567b2
Dean321 <parvathi.saxena@gmail.com>
2023-02-26 13:33:20 +0530

    Update SoundButton.js

--- Wireless Buzzer/quiz-buzzer/components/SoundButton.js
+++ Wireless Buzzer/quiz-buzzer/components/SoundButton.js
@@ -1,59 +1,66 @@
 import React, { Component } from 'react';
 import { View, Button, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { Audio } from 'expo-av';
-import db from "../config";
-import firebase from "firebase"
+import db from '../config';
+import firebase from 'firebase';
 const sound = new Audio.Sound();
 
-class SoundButton extends Component{
+class SoundButton extends Component {
   startSound = async () => {
     await sound.loadAsync(require('../assets/Buzzer.mp3'));
     await sound.playAsync();
-  }
+  };
   stopSound = async () => {
     await sound.unloadAsync();
-  }
-  isButtonPressed(buttonColor){
-    var ts =firebase.database.ServerValue.TIMESTAMP; //new Date().toISOString();
-    console.log("ts => ",ts)
-    var team = db.ref("teams/"+buttonColor+"/");
+  };
+  isButtonPressed(buttonColor) {
+    var ts = firebase.database.ServerValue.TIMESTAMP; //new Date().toISOString();
+    console.log('ts => ', ts);
+    var team = db.ref('teams/' + buttonColor + '/');
     team.update({
-      "ibp":true,
-      "ts":ts
-    })
+      ibp: true,
+      ts: ts,
+    });
   }
   render() {
+    var color = 'white';
+    if (this.props.color == 'orange') color = '#F2CD5C';
+    else if (this.props.color == 'red') color = '#E96479';
+    else if (this.props.color == 'green') color = '#7DB9B6';
+    else color = 'lightblue';
     return (
-      <View>
-        <TouchableOpacity style={[styles.button, {backgroundColor:this.props.color}]} onPress={()=>{
-          var buttonColor = this.props.color
-          this.isButtonPressed(buttonColor)
-          this.startSound()
-        }}>
+      <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+        <TouchableOpacity
+          style={[styles.button, { backgroundColor: color }]}
+          onPress={() => {
+            var buttonColor = this.props.color;
+            this.isButtonPressed(buttonColor);
+            this.startSound();
+          }}>
           <Text style={styles.buttonText}>Play</Text>
         </TouchableOpacity>
-        </View>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   button: {
-        marginLeft:50,
-        marginTop:50,
-        width:200,
-        height:200,
-        backgroundColor:"red",
-        borderRadius:200,
-        borderColor: "blue",
-        alignItems:"center",
-        justifyContent:"center" 
+    // marginLeft:50,
+    // marginTop:50,
+    width: 200,
+    height: 200,
+    backgroundColor: 'red',
+    borderRadius: 200,
+    borderColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  buttonText:{
-        fontSize:35, 
-        fontWeight:"bold", 
-        color:"white" 
-  }
+  buttonText: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    color: 'white',
+  },
 });
 
 export default SoundButton;
