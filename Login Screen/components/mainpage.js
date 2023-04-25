ec817d6cd9718c428c87736d93e60170cbdc409c
Dean321 <parvathi.saxena@gmail.com>
2023-04-25 14:43:40 +0530

    Update mainpage.js

--- Login Screen/components/mainpage.js
+++ Login Screen/components/mainpage.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 export default class MainPage extends React.Component {
   constructor() {
     super();
     this.state = {
       theme: 'light',
+      pimg:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+      fn:"",
+      ln:""
     };
   }
   getTheme() {
-    this.setState({ theme: this.props.navigation.state.params.theme });
+    this.setState({ theme: this.props.navigation.state.params.theme,
+    pimg: this.props.navigation.state.params.pimg,
+    fn: this.props.navigation.state.params.fn,
+    ln:this.props.navigation.state.params.ln
+      });
   }
   componentDidMount(){
     this.getTheme()
@@ -24,7 +31,12 @@ export default class MainPage extends React.Component {
           justifyContent: 'center',
           alignItems: 'center',
         },{backgroundColor:this.state.theme=="dark"?"grey":"white"}]}>
-        <Text>Main Page</Text>
+        <Text>Profile Page</Text>
+        <Image source={{uri:this.state.pimg}} style={{width:200, height:200}}/>
+        <View style={{flexDirection:"row"}}>
+        <Text>{this.state.fn+"\t"}</Text>
+        <Text>{this.state.ln}</Text>
+        </View>
       </View>
     );
   }
